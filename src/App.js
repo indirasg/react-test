0ac5993fadc5b19795b40920c273404cb98375db
IndiraMohan <indira1291@gmail.com>
2022-12-11 13:28:34 +0800

    Modified Text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome <code>to</code>Appmart 
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Welcome to AppMart!
+          Landingpage to AppMart!
         </a>
       </header>
     </div>
