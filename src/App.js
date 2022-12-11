55412dcbf982f404469f71f352b545364b7a2eb7
IndiraMohan <indira1291@gmail.com>
2022-12-11 12:53:30 +0800

    testing

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Welcome to AppMart!!!
+          Welcome to AppMart!
         </a>
       </header>
     </div>
