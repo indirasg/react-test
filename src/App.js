9ab54140bd8716e12545deb6de543af88fa11f8c
IndiraMohan <indira1291@gmail.com>
2022-12-12 17:46:27 +0800

    Removed the space

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome <code>to</code>Appmart 
+          Welcome <code>to</code> Appmart 
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Landingpage to AppMart!
+        AppMart!
         </a>
       </header>
     </div>
